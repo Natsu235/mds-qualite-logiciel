9d87831c6bcd48fc4f3acefb6d1c4aa5149c3e1d
Natsu235 <27864198+Natsu235@users.noreply.github.com>
2020-04-09 10:41:51 +0200

    Création et annotation des controleurs

---
+++ monprojetsb/src/main/java/com/tactfactory/monprojetsb/monprojetsb/controllers/ProductController.java
@@ -0,0 +1,43 @@
+package com.tactfactory.monprojetsb.monprojetsb.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tactfactory.monprojetsb.monprojetsb.repositories.ProductRepository;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+	@Autowired ProductRepository repository;
+
+	// Liste l'ensemble des attributs de l’entité pour tout les enregistrements
+	public void index() {
+		// TODO
+	}
+
+	// Affiche la vue de création d'un élément
+	@GetMapping
+    public void createGet() {
+		// TODO
+	}
+
+    // Envoie les informations de l'élément vers le serveur et le sauvegarde dans la base de données
+	@PostMapping
+    public void createPost() {
+		// TODO
+    }
+
+    // Supprime un élément de la base de données
+    public void delete() {
+    	// TODO
+    }
+
+    // Affiche les détails d’un élément
+    public void details() {
+    	// TODO
+    }
+}
